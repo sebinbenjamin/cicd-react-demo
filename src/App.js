740960516f16e266c994f779b9dc514bc8bba38c
Sebin Benjamin <sebinbenjamin@gmail.com>
2021-02-10 12:25:06 +1300

    add breaking unit tests

--- src/App.js
+++ src/App.js
@@ -8,8 +8,18 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export defaultsdsa App;
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Addition 1', () => {
+  expect(3 + 2).toBe(5);
+});
+
+test('Addition 2', () => {
+  expect(3 + 2).toBe(6);
+});
